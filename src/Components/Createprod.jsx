9a59142d721bccb36b4e62cadeb0e174b79cc275
Mohit Sharma <mohitsharma90015656@gmail.com>
2022-09-10 14:03:34 +0530

    Done

---
+++ src/Components/Createprod.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export const Createprod = () => {
+    const [image,setImg] = React.useState("");
+    const [title,setTitle] = React.useState("");
+    const [price,setPrice] = React.useState("")
+
+
+    const addProduct= ()=>{
+        const paylode = {
+                    image:image,
+                    title : title,
+                    price : price,
+                }
+       fetch(`http://localhost:3002/products`,{
+        method : "POST",
+        body : JSON.stringify(paylode),
+        headers : {
+          "Content-Type" : "application/json"
+        }
+       })
+    
+      
+    }
+  return (
+    
+    <div>
+        
+            <label>Image Url : </label>
+            <input type="text" placeholder='Enter Url' onChange={(e)=>setImg(e.target.value)} value={image}/><br /><br />
+            <label>Title : </label>
+            <input type="text" placeholder='Enter Title' onChange={(e)=>setTitle(e.target.value)} value={title}/><br /><br />
+            {/* <label>Gender : </label>
+            <input type="text" placeholder='Enter Gender'/><br /><br /> */}
+            <label >Price : </label>
+            <input type="text" placeholder='Enter Price' onChange={(e)=>setPrice(e.target.value)} value={price}/><br /><br />
+            {/* <Link to=""><input type="submit" value="Create Product" style={{backgroundColor : "black",color : "white"}}/></Link> */}
+            <Link to="/"><button onClick={addProduct} style={{backgroundColor : "black",color : "white"}}>Add Product</button></Link>
+     
+    </div>
+  )
+}
